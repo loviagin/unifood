cde9f21b964eb2022cb55d59c40d0c9ea4dd6ffe
Ilia <ilia.loviagin@gmail.com>
2025-03-05 18:46:00 +0300

    Added (root) folder and Home page

---
+++ app/(root)/layout.jsx
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Uni food plus",
+  description: "Программа лояльности Uni Food в РТУ МИРЭА",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
