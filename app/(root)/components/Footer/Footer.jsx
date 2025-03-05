730ed59eff7fb8d689b796f530df0d84e4d89283
Ilia <ilia.loviagin@gmail.com>
2025-03-05 18:49:21 +0300

    Added Footer component

---
+++ app/(root)/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            Uni Cafe plus. Все права защищены 2025 ©
+        </footer>
+    )
+}
+
+export default Footer
