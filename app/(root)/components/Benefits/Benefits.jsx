792a3f400d10c6ef06076a5c7dfc7cd8c964d02e
Ilia <ilia.loviagin@gmail.com>
2025-03-05 19:12:10 +0300

    Added desktop and mobile optimization

--- app/(root)/components/Benefits/Benefits.jsx
+++ app/(root)/components/Benefits/Benefits.jsx
@@ -3,22 +3,42 @@ import Image from "next/image";
 
 const Benefits = () => {
     return (
-        <section className={styles.benefits}>
+        <section className={styles.benefits} id="more">
             <h2>Что вы получите</h2>
             <p>Присоединившись к программе</p>
 
-            {[1, 2, 3].map((num) => (
-                <div key={num} className={styles.benefitCard}>
-                    <Image
-                        src="/benefit-icon.png"
-                        alt="Benefit icon"
-                        width={104}
-                        height={96}
-                    />
-                    <h3>Преимущество #{num}</h3>
-                    <p>Я получу что-то очень {num === 1 ? 'крутое' : num === 2 ? 'вкусное' : 'выгодное'} ммм . . .</p>
-                </div>
-            ))}
+            <div className={styles.benefitCard}>
+                <Image
+                    src="/cup-1.png"
+                    alt="Benefit icon"
+                    width={104}
+                    height={96}
+                />
+                <h3>Система уровней</h3>
+                <p>Чем больше вы покупаете, тем выше уровень. С каждым новым уровнем увеличивается процент кешбека</p>
+            </div>
+
+            <div className={styles.benefitCard}>
+                <Image
+                    src="/cup-2.png"
+                    alt="Benefit icon"
+                    width={104}
+                    height={96}
+                />
+                <h3>Покупайте за баллы</h3>
+                <p>Покажите QR на кассе и покупайте с большой скидкой или даже бесплатно</p>
+            </div>
+
+            <div className={styles.benefitCard}>
+                <Image
+                    src="/cup-3.png"
+                    alt="Benefit icon"
+                    width={104}
+                    height={96}
+                />
+                <h3>История покупок</h3>
+                <p>Просматривайте когда и что вы купили, а также полученную сумму кешбека</p>
+            </div>
         </section>
     )
 }
