7aaaf3071fec8c95a216d53a12edb87e029dbf0a
Ilia <ilia.loviagin@gmail.com>
2025-03-10 15:21:55 +0300

    Added new api /orders/

--- app/models/Order.js
+++ app/models/Order.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const OrderSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     title: { type: String, required: true },
-    details: { type: String, required: true },
+    details: { type: String, default: '' },
     amount: { type: Number, required: true },
     date: { type: Date, required: true },
     bonuses: { type: Number, required: true },
