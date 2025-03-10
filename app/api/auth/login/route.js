892a4be8bcf811797b77f709c18d1a1903277bc2
Ilia <ilia.loviagin@gmail.com>
2025-03-10 11:40:08 +0300

    New api auth users

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import connectDB from "../db";
-import User from "../../models/User";
+import connectDB from "../../db";
+import User from "../../../models/User";
 import bcrypt from "bcryptjs";
 
 export async function POST(request) {
