310e084302a0e2f09264c819e0afb7c065b946e7
Ilia <ilia.loviagin@gmail.com>
2025-03-10 15:24:35 +0300

    Added new api /orders/

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -1,9 +1,6 @@
 //app/api/users/route.js
 import { NextResponse } from "next/server";
 import connectDB from "../db";
-import User from "../../models/User";
-import UserData from "../../models/UserData";
-import bcrypt from "bcryptjs";
 import Order from "../../models/Order";
 import { Types } from "mongoose";
 
@@ -34,41 +31,12 @@ export async function POST(request) {
                 userId: Types.ObjectId.createFromHexString(userId) 
             }).sort({ date: -1 });
 
-            return NextResponse.json(orders, { status: 200 });
+            return NextResponse.json({ history: orders || [] }, { status: 200 });
 
         } catch (error) {
             console.error("Ошибка при обработке userId:", error);
             return NextResponse.json({ error: "Некорректный формат userId" }, { status: 400 });
         }
-    } catch (error) {
-        console.error("Ошибка при загрузке заказов:", error);
-        return NextResponse.json({ error: "Ошибка сервера" }, { status: 500 });
-    }
-}
-
-export async function GET(request) {
-    await connectDB();
-
-    try {
-        const { searchParams } = new URL(request.url);
-        const userId = searchParams.get('userId');
-
-        if (!userId) {
-            return NextResponse.json({ error: "userId обязателен" }, { status: 400 });
-        }
-
-        try {
-            const orders = await Order.find({
-                userId: Types.ObjectId.createFromHexString(userId)
-            }).sort({ date: -1 });
-
-            return NextResponse.json(orders, { status: 200 });
-
-        } catch (error) {
-            console.error("Ошибка при обработке userId:", error);
-            return NextResponse.json({ error: "Некорректный формат userId" }, { status: 400 });
-        }
-
     } catch (error) {
         console.error("Ошибка при загрузке заказов:", error);
         return NextResponse.json({ error: "Ошибка сервера" }, { status: 500 });
